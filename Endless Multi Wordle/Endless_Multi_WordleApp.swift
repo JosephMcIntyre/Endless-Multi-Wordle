46c43f05b2c39c8f2157caeb376c4705dc2ef9b6
“JosephMcIntyre” <Joseph_McIntyre@icloud.com>
2022-09-03 15:30:32 -0400

    Initial Commit

---
+++ Endless Multi Wordle/Endless_Multi_WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Endless_Multi_WordleApp.swift
+//  Endless Multi Wordle
+//
+//  Created by Joseph McIntyre on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Endless_Multi_WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
