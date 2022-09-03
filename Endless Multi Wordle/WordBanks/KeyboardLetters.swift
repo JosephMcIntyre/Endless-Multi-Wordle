7b6db1f623c10ae098a94256ba89f3f81e84e6eb
“JosephMcIntyre” <Joseph_McIntyre@icloud.com>
2022-09-03 15:59:37 -0400

    Update WordBanks

---
+++ Endless Multi Wordle/WordBanks/KeyboardLetters.swift
@@ -0,0 +1,37 @@
+//
+//  KeyboardLetters.swift
+//  Endless Multi Wordle
+//
+//  Created by Joseph McIntyre on 9/3/22.
+//
+
+import Foundation
+
+let KBLetters = [
+    Letter(char: "q"),
+    Letter(char: "w"),
+    Letter(char: "e"),
+    Letter(char: "r"),
+    Letter(char: "t"),
+    Letter(char: "y"),
+    Letter(char: "u"),
+    Letter(char: "i"),
+    Letter(char: "o"),
+    Letter(char: "p"),
+    Letter(char: "a"),
+    Letter(char: "s"),
+    Letter(char: "d"),
+    Letter(char: "f"),
+    Letter(char: "g"),
+    Letter(char: "h"),
+    Letter(char: "j"),
+    Letter(char: "k"),
+    Letter(char: "l"),
+    Letter(char: "z"),
+    Letter(char: "x"),
+    Letter(char: "c"),
+    Letter(char: "v"),
+    Letter(char: "b"),
+    Letter(char: "n"),
+    Letter(char: "m"),
+]
